6f930911e28e48f3bfa5b659ad9001fdd391f3d2
Vindevogel Victor <victor.vindevogel@epitech.eu>
2024-03-04 12:22:57 +0100

    [+] naive bayes with simple manual preprocessing

--- naive_bayes/naive_bayes.py
+++ naive_bayes/naive_bayes.py
@@ -1,25 +1,47 @@
 import sys
-
+import string
+import re
 sys.path.append("..")
 
+
 from data_loading import get_spam_dataset
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+
+
 
 df = get_spam_dataset()
 # print(df.info())
 
-from sklearn.model_selection import train_test_split
+def simple_preprocess_text(text):
+    # Remove 'Subject:' and any non-letter characters, convert to lowercase
+    text = re.sub(r'Subject:', '', text)
+    text = re.sub(r'[^a-zA-Z]', ' ', text)
+    text = text.lower()
+
+    # Remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+
+    # Remove stopwords
+    tokens = text.split()
+    tokens = [word for word in tokens if word not in ENGLISH_STOP_WORDS]
+
+    # Join tokens back to string
+    text = ' '.join(tokens)
+
+    return text
+
+# Apply simplified text preprocessing
+df['Body'] = df['Body'].apply(simple_preprocess_text)
 
 X_train, X_test, y_train, y_test = train_test_split(df['Body'], df['Label'], test_size=0.2, random_state=42)
 
-from sklearn.pipeline import make_pipeline
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
-
 model = make_pipeline(TfidfVectorizer(), MultinomialNB())
 model.fit(X_train, y_train)
-
-from sklearn.metrics import classification_report, accuracy_score
-
 
 predictions = model.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
@@ -30,3 +52,4 @@ report = classification_report(y_test, predictions)
 print(f'Accuracy: {accuracy}')
 print(f'Train accuracy: {train_accuracy}')
 print(report)
+
